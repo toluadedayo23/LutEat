aa9bc18bb0ee73591c2a49a9bf2d395b0af91315
Toluadedayo23 <toluadedayo23@gmail.com>
2022-10-20 23:25:38 +0100

    Global exception Handler

---
+++ src/main/java/com/practise/luteat/exceptions/handler/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.practise.luteat.exceptions.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
