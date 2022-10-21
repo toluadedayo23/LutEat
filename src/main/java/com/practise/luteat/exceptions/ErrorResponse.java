586e26c45cf6d61c97a703ef84e59eb6a85f0bc1
Toluadedayo23 <toluadedayo23@gmail.com>
2022-10-21 22:53:33 +0100

    Added ErrorResponse and GlobalExceptionHandler

--- src/main/java/com/practise/luteat/exceptions/ErrorResponse.java
+++ src/main/java/com/practise/luteat/exceptions/ErrorResponse.java
@@ -1,2 +1,31 @@
-package com.practise.luteat.exceptions;public class ErrorResponse {
+package com.practise.luteat.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+public class ErrorResponse {
+
+    private Date timestamp;
+    private int code;
+    private String status;
+    private String message;
+
+    public ErrorResponse(){
+        this.timestamp = new Date();
+
+    }
+
+    public ErrorResponse(HttpStatus httpStatus, String message){
+        this.timestamp = new Date();
+        this.code = httpStatus.value();
+        this.status = httpStatus.name();
+        this.message = message;
+    }
 }
--- src/main/java/com/practise/luteat/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/practise/luteat/exceptions/handler/GlobalExceptionHandler.java
@@ -1,11 +1,90 @@
 package com.practise.luteat.exceptions.handler;
 
+import com.practise.luteat.exceptions.EmailVerificationException;
+import com.practise.luteat.exceptions.ErrorResponse;
+import com.practise.luteat.exceptions.UsernameEmailExistsException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EmailVerificationException.class)
+    public ResponseEntity<ErrorResponse> handleEmailVerificationException(Exception e) {
+
+        EmailVerificationException userEmailVerificationException = (EmailVerificationException) e;
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        log.error("EmailVerification Exception Error");
+
+        return new ResponseEntity<>(new ErrorResponse(status, e.getMessage()), status);
+
+    }
+
+    @ExceptionHandler(UsernameEmailExistsException.class)
+    public ResponseEntity<ErrorResponse> handleUsernameEmailExistsException(Exception e) {
+
+        UsernameEmailExistsException usernameEmailExistsException = (UsernameEmailExistsException) e;
+
+        HttpStatus status = HttpStatus.NOT_ACCEPTABLE;
+
+        log.error("Username or Email already exists");
+
+        return new ResponseEntity<>(new ErrorResponse(status, e.getMessage()), status);
+
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUsernameNotFoundException(UsernameNotFoundException e) {
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        log.error("Username not found in the UserDetailsService");
+
+        return new ResponseEntity<>(new ErrorResponse(status, e.getMessage()), status);
+    }
+
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
+
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+
+        log.error("Authentication failure");
+
+        return new ResponseEntity<>(new ErrorResponse(status, e.getMessage()), status);
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        List<String> validationList = ex.getBindingResult().getFieldErrors().stream().map(fieldError -> fieldError.getDefaultMessage()).collect(Collectors.toList());
+        log.info("Validation error: " + validationList);
+        return new ResponseEntity<>(new ErrorResponse(status, errorMessage), status);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        log.error("Internal Error");
+
+        return new ResponseEntity<>(new ErrorResponse(status, e.getMessage()), status);
+    }
 
 }
