4b0df02f2970e1b289822f853dd1b3f4299f45c6
Toluadedayo23 <toluadedayo23@gmail.com>
2022-10-22 14:22:18 +0100

    Added a some new Dto

--- src/main/java/com/practise/luteat/dto/AuthenticationResponse.java
+++ src/main/java/com/practise/luteat/dto/AuthenticationResponse.java
@@ -1,7 +1,14 @@
 package com.practise.luteat.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.Instant;
 
+@Data
+@Builder
 public class AuthenticationResponse {
     private String authenticationToken;
     private String refreshToken;
