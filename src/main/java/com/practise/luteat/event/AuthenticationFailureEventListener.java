6d9924da66d081911783b306023cf26f21c63a32
Toluadedayo23 <toluadedayo23@gmail.com>
2022-10-24 22:09:15 +0100

    Listener for login attempts

---
+++ src/main/java/com/practise/luteat/event/AuthenticationFailureEventListener.java
@@ -0,0 +1,20 @@
+package com.practise.luteat.event;
+
+import com.practise.luteat.service.impl.LoginAttemptService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthenticationFailureEventListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+    private final LoginAttemptService loginAttemptService;
+
+    @Override
+    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent event) {
+        String username = event.getAuthentication().getName();
+        loginAttemptService.loginFailed(username);
+    }
+}
---
+++ src/main/java/com/practise/luteat/event/AuthenticationSuccessEventListener.java
@@ -0,0 +1,19 @@
+package com.practise.luteat.event;
+
+import com.practise.luteat.service.impl.LoginAttemptService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthenticationSuccessEventListener implements ApplicationListener<AuthenticationSuccessEvent> {
+
+    private final LoginAttemptService loginAttemptService;
+    @Override
+    public void onApplicationEvent(AuthenticationSuccessEvent event) {
+        String username = event.getAuthentication().getName();
+        loginAttemptService.loginSuccess(username);
+    }
+}
