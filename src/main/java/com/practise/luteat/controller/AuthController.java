f76d16ad5b07a7480f9bcc844c20a506932a93bb
Toluadedayo23 <toluadedayo23@gmail.com>
2022-10-24 19:24:21 +0100

    Auth Controller

--- src/main/java/com/practise/luteat/controller/AuthController.java
+++ src/main/java/com/practise/luteat/controller/AuthController.java
@@ -1,17 +1,20 @@
 package com.practise.luteat.controller;
 
-import com.practise.luteat.dto.RegisterRequest;
+import com.practise.luteat.dto.*;
 import com.practise.luteat.event.UserRegistrationEvent;
+import com.practise.luteat.listener.EmailSender;
 import com.practise.luteat.model.User;
 import com.practise.luteat.service.AuthService;
 import com.practise.luteat.service.UserEmailVerificationService;
+import com.practise.luteat.service.impl.RefreshTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -21,18 +24,42 @@ public class AuthController {
     private final AuthService authService;
     private final UserEmailVerificationService userEmailVerificationService;
     private final ApplicationEventPublisher applicationEventPublisher;
+    private final EmailSender emailSender;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@Valid @RequestBody RegisterRequest registerRequest){
-        User user = authService.singUp(registerRequest);
+    public ResponseEntity<String> signup(@Valid @RequestBody signupRequest signupRequest) {
+        User user = authService.singUp(signupRequest);
         applicationEventPublisher.publishEvent(new UserRegistrationEvent(user));
-        return ResponseEntity.status(HttpStatus.OK).body("User Registration Successful");
+        return ResponseEntity.status(OK).body("User Registration Successful");
     }
 
     @GetMapping("/accountVerification/{token}")
-    public ResponseEntity<String> verifyAccount(@PathVariable("token") String token){
+    public ResponseEntity<String> verifyAccount(@PathVariable("token") String token) {
         userEmailVerificationService.verifyAccount(token);
-        return ResponseEntity.status(HttpStatus.OK).body("Account Verification Successful");
+        return ResponseEntity.status(OK).body("Account Verification Successful");
     }
 
+    @PostMapping("/resendVerificationEmail")
+    public ResponseEntity<String> resendVerificationEmail(@Valid @RequestBody ResendVerificationDetailsDto resendVerificationDetailsDto) {
+        User user = emailSender.resendVerificationLink(resendVerificationDetailsDto);
+        applicationEventPublisher.publishEvent(new UserRegistrationEvent(user));
+        return ResponseEntity.status(OK).body("Verification Email has been resent");
+    }
+
+    @PostMapping("/login")
+    public AuthenticationResponse login(@Valid @RequestBody LoginRequest loginRequest) {
+        return authService.login(loginRequest);
+    }
+
+    @PostMapping("/refreshToken")
+    public AuthenticationResponse refreshToken(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        return authService.refreshToken(refreshTokenRequest);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+        return ResponseEntity.status(OK).body("User logout!");
+    }
 }
