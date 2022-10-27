6d3c5fb3889456b71044916b7f7e3e02d84aee49
toluadedayo23 <toluadedayo23@gmail.com>
2022-10-27 14:23:56 +0100

    Service

--- src/main/java/com/practise/luteat/service/MenuOrderService.java
+++ src/main/java/com/practise/luteat/service/MenuOrderService.java
@@ -1,5 +1,17 @@
 package com.practise.luteat.service;
 
+import com.practise.luteat.dto.MenuOrderDto;
+
+import java.util.List;
+
 public interface MenuOrderService {
+
+    List<MenuOrderDto> getAllMenu();
+
+    MenuOrderDto UpdateMenu(MenuOrderDto menuOrderDto);
+
+    void DeleteMenu(MenuOrderDto menuOrderDto);
+
+    MenuOrderDto createMenu(MenuOrderDto menuOrderDto);
 
 }
--- src/main/java/com/practise/luteat/service/OrderService.java
+++ src/main/java/com/practise/luteat/service/OrderService.java
@@ -1,4 +1,5 @@
 package com.practise.luteat.service;
 
 public interface OrderService {
+
 }
