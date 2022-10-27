c8376740d1de74f51af2acc315d096e0f0db3696
toluadedayo23 <toluadedayo23@gmail.com>
2022-10-27 14:23:01 +0100

    Repo

--- src/main/java/com/practise/luteat/repository/MenuOrderRepository.java
+++ src/main/java/com/practise/luteat/repository/MenuOrderRepository.java
@@ -1,7 +1,13 @@
 package com.practise.luteat.repository;
 
+import com.practise.luteat.dto.MenuOrderDto;
 import com.practise.luteat.model.MenuOrders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MenuOrderRepository extends JpaRepository<MenuOrders, Long> {
+    Optional<MenuOrders> findByName(String name);
+
+    boolean existsByName(String name);
 }
