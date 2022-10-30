b9d27380eac943bbd5452ced0e07f8a3bc7783af
Toluadedayo23 <toluadedayo23@gmail.com>
2022-10-30 11:12:33 +0100

    ControllerAdvice -> RestControllerAdvice

--- src/main/java/com/practise/luteat/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/practise/luteat/exceptions/handler/GlobalExceptionHandler.java
@@ -5,36 +5,37 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EmailVerificationException.class)
-    public ResponseEntity<ErrorResponse> handleEmailVerificationException(Exception e) {
+    public ErrorResponse handleEmailVerificationException(Exception e) {
 
         EmailVerificationException userEmailVerificationException = (EmailVerificationException) e;
         HttpStatus status = HttpStatus.NOT_FOUND;
 
         log.error("EmailVerification Exception Error");
 
-        return new ResponseEntity<>(new ErrorResponse(status, userEmailVerificationException.getMessage()), status);
+        return new ErrorResponse(status, userEmailVerificationException.getMessage());
 
     }
 
     @ExceptionHandler(UsernameEmailExistsException.class)
-    public ResponseEntity<ErrorResponse> handleUsernameEmailExistsException(Exception e) {
+    public ErrorResponse handleUsernameEmailExistsException(Exception e) {
 
         UsernameEmailExistsException usernameEmailExistsException = (UsernameEmailExistsException) e;
 
@@ -42,29 +43,29 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
         log.error("Username or Email already exists");
 
-        return new ResponseEntity<>(new ErrorResponse(status, usernameEmailExistsException.getMessage()), status);
+        return new ErrorResponse(status, usernameEmailExistsException.getMessage());
 
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleUsernameNotFoundException(UsernameNotFoundException e) {
+    public ErrorResponse handleUsernameNotFoundException(UsernameNotFoundException e) {
 
         HttpStatus status = HttpStatus.NOT_FOUND;
 
         log.error("Username not found in the UserDetailsService");
 
-        return new ResponseEntity<>(new ErrorResponse(status, e.getMessage()), status);
+        return new ErrorResponse(status, e.getMessage());
     }
 
 
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
+    public ErrorResponse handleAuthenticationException(AuthenticationException e) {
 
         HttpStatus status = HttpStatus.UNAUTHORIZED;
 
-        log.error("Authentication failure");
+        log.error("Authentication failure, user not authenticated");
 
-        return new ResponseEntity<>(new ErrorResponse(status, e.getMessage()), status);
+        return new ErrorResponse(status, e.getMessage());
     }
 
 
@@ -77,37 +78,44 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(RefreshTokenException.class)
-    public ResponseEntity<ErrorResponse> handleRefreshTokenException(Exception e){
+    public ErrorResponse handleRefreshTokenException(Exception e){
         RefreshTokenException refreshTokenException = (RefreshTokenException) e;
         HttpStatus status = HttpStatus.NOT_FOUND;
         log.error("Invalid Refresh Token");
-        return new ResponseEntity<>(new ErrorResponse(status, refreshTokenException.getMessage()), status);
+        return new ErrorResponse(status, refreshTokenException.getMessage());
     }
 
     @ExceptionHandler(LockedException.class)
-    public ResponseEntity<ErrorResponse> handleLockedException(LockedException e){
+    public ErrorResponse handleLockedException(LockedException e){
         HttpStatus status = HttpStatus.LOCKED;
         log.error("User account has been locked for 24 hours");
-        return new ResponseEntity<>(new ErrorResponse(status, e.getMessage()), status);
+        return new ErrorResponse(status, e.getMessage());
     }
 
     @ExceptionHandler(MenuOrderException.class)
-    public ResponseEntity<ErrorResponse> handleMenuOrderException(Exception e){
+    public ErrorResponse handleMenuOrderException(Exception e){
         MenuOrderException menuOrderException = (MenuOrderException) e;
         HttpStatus status = HttpStatus.NOT_FOUND;
         log.error("Menu Not Found");
-        return ResponseEntity.status(status).body(new ErrorResponse(status, menuOrderException.getMessage()));
+        return new ErrorResponse(status, menuOrderException.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ErrorResponse handleAccessDeniedException(AccessDeniedException e){
+        HttpStatus status = HttpStatus.FORBIDDEN;
+        log.error("User does not have the proper permission to access this resource");
+        return new ErrorResponse(status, e.getMessage());
     }
 
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+    public ErrorResponse handleException(Exception e) {
 
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
         log.error("Internal Error");
 
-        return new ResponseEntity<>(new ErrorResponse(status, e.getMessage()), status);
+        return new ErrorResponse(status, e.getMessage());
     }
 
 }
