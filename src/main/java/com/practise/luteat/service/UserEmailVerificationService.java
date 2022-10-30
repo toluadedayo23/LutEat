fe06cbc913eef6828c1f19b91d854a2f86e0d8e9
Toluadedayo23 <toluadedayo23@gmail.com>
2022-10-30 11:31:33 +0100

    Service

--- src/main/java/com/practise/luteat/service/UserEmailVerificationService.java
+++ src/main/java/com/practise/luteat/service/UserEmailVerificationService.java
@@ -1,7 +1,10 @@
 package com.practise.luteat.service;
 
+import com.practise.luteat.dto.ResendVerificationDetailsDto;
+
 public interface UserEmailVerificationService {
 
     void verifyAccount(String token);
     String generateVerificationTokenByUsername(String username);
+    void resendVerificationLink(ResendVerificationDetailsDto resendVerificationDetailsDto);
 }
