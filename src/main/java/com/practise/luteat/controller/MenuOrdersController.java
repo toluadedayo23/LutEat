94de6dd30cabc52168f655e84ff53ab86c9a8f80
Toluadedayo23 <toluadedayo23@gmail.com>
2022-10-30 13:35:26 +0100

    Controllers

--- src/main/java/com/practise/luteat/controller/MenuOrdersController.java
+++ src/main/java/com/practise/luteat/controller/MenuOrdersController.java
@@ -1,9 +1,40 @@
 package com.practise.luteat.controller;
 
+import com.practise.luteat.dto.MenuOrderDto;
+import com.practise.luteat.service.MenuOrderService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/menuOrders")
 public class MenuOrdersController {
+
+    private final MenuOrderService menuOrderService;
+
+    @GetMapping("/get-all-orders")
+    public ResponseEntity<List<MenuOrderDto>> getAllMenu(){
+        return ResponseEntity.status(HttpStatus.OK).body(menuOrderService.getAllMenu());
+    }
+
+    @PostMapping("/update-menu")
+    public ResponseEntity<MenuOrderDto> updateMenu(@Valid @RequestBody MenuOrderDto menuOrderDto){
+        return ResponseEntity.status(HttpStatus.OK).body(menuOrderService.updateMenu(menuOrderDto));
+    }
+
+    @PostMapping("/delete-menu")
+    public ResponseEntity<String>deleteMenu(@Valid @RequestBody MenuOrderDto menuOrderDto){
+        menuOrderService.deleteMenu(menuOrderDto);
+        return new ResponseEntity<>("Menu has been successfully deleted", HttpStatus.OK);
+    }
+
+    @PostMapping("/create-menu")
+    public ResponseEntity<MenuOrderDto> createMenu(@Valid @RequestBody MenuOrderDto menuOrderDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(menuOrderService.createMenu(menuOrderDto));
+    }
 }
--- src/main/java/com/practise/luteat/controller/OrdersController.java
+++ src/main/java/com/practise/luteat/controller/OrdersController.java
@@ -1,9 +1,39 @@
 package com.practise.luteat.controller;
 
+import com.practise.luteat.dto.CreateOrderDto;
+import com.practise.luteat.dto.CreateOrderResponse;
+import com.practise.luteat.dto.OrderBYDateDto;
+import com.practise.luteat.dto.OrderResponse;
+import com.practise.luteat.service.OrdersService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/orders")
 public class OrdersController {
+
+    private final OrdersService ordersService;
+
+    @GetMapping("/get-recent-orders-by/{username}")
+    public ResponseEntity<List<OrderResponse>> getRecentOrdersByUsername(@PathVariable("username") String username) {
+        return ResponseEntity.status(HttpStatus.OK).body(ordersService.getRecentOrdersByUsername(username));
+    }
+
+    @PostMapping("/create-order")
+    public ResponseEntity<CreateOrderResponse> createOrder(@Valid @RequestBody CreateOrderDto createOrderDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(ordersService.createOrder(createOrderDto));
+    }
+
+
+    @PostMapping("/get-order-by-date")
+    public ResponseEntity<List<OrderResponse>> getOrderByDate(@Valid @RequestBody OrderBYDateDto orderBYDateDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(ordersService.getOrderByDate(orderBYDateDto));
+    }
+
 }
