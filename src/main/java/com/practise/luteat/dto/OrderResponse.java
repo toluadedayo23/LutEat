23594f7f3777c6d0093de726e8964fdc04599a34
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-02 19:52:17 +0100

    dto

--- src/main/java/com/practise/luteat/dto/OrderResponse.java
+++ src/main/java/com/practise/luteat/dto/OrderResponse.java
@@ -1,14 +1,2 @@
-package com.practise.luteat.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import java.util.Date;
-
-@Data
-@AllArgsConstructor
-public class OrderResponse {
-    private String menuName;
-    private Double menuPrice;
-    private Date dateCreated;
+package com.practise.luteat.dto;public class OrderResponse {
 }
