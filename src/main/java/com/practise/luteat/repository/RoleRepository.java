8bb14298c12216c10072f2f332a45c9659ac0df1
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-03 05:51:07 +0100

    Repo

---
+++ src/main/java/com/practise/luteat/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.practise.luteat.repository;
+
+import com.practise.luteat.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
