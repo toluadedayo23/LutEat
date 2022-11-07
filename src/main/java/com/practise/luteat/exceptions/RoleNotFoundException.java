20716286c840a680fd4c1471be17629ce75f0075
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-07 09:33:18 +0100

    Exeptions

---
+++ src/main/java/com/practise/luteat/exceptions/RoleNotFoundException.java
@@ -0,0 +1,8 @@
+package com.practise.luteat.exceptions;
+
+public class RoleNotFoundException extends RuntimeException{
+
+    public RoleNotFoundException(String message){
+         super(message);
+    }
+}
