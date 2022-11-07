257b70feb6d75dcf2d464a70fc80bbfa3335eb02
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-07 09:40:46 +0100

    Repo

--- src/main/java/com/practise/luteat/repository/RoleRepository.java
+++ src/main/java/com/practise/luteat/repository/RoleRepository.java
@@ -1,7 +1,11 @@
 package com.practise.luteat.repository;
 
+import com.practise.luteat.model.ERole;
 import com.practise.luteat.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByNames(ERole role);
 }
