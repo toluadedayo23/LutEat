dc3cc28d09857da519a23db29548389c358eec4c
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-09 13:09:48 +0100

    dto

--- src/main/java/com/practise/luteat/dto/CreateOrderResponse.java
+++ src/main/java/com/practise/luteat/dto/CreateOrderResponse.java
@@ -1,16 +1,19 @@
 package com.practise.luteat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.Instant;
+import java.util.Date;
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class CreateOrderResponse {
     private String username;
     private Double totalPrice;
-    private Instant orderDate;
+    private Date orderDate;
     private List<MenuOrderDto> order;
 }
