05371e22d578e09a876e4e017ceac269a7429c19
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-09 13:10:14 +0100

    Mapper

--- src/main/java/com/practise/luteat/mapper/OrdersMapper.java
+++ src/main/java/com/practise/luteat/mapper/OrdersMapper.java
@@ -1,6 +1,8 @@
 package com.practise.luteat.mapper;
 
 import com.practise.luteat.dto.CreateOrderDto;
+import com.practise.luteat.dto.MenuOrderDto;
+import com.practise.luteat.exceptions.OrderException;
 import com.practise.luteat.model.Orders;
 import com.practise.luteat.model.User;
 import com.practise.luteat.repository.UserRepository;
@@ -9,6 +11,8 @@ import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Date;
+
 @Mapper(componentModel = "spring")
 public abstract class OrdersMapper {
 
@@ -16,11 +20,11 @@ public abstract class OrdersMapper {
     private UserRepository userRepository;
 
 
-
     @Mapping(target = "orderId", ignore = true)
     @Mapping(target = "user", expression = "java(findUser(orderDto.getUsername()))")
-    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "createdDate", expression = "java(returnDate())")
     @Mapping(target = "orders", source = "menuList")
+    @Mapping(target = "totalPrice", expression = "java(getTotalOrderPrice(orderDto))")
     public abstract Orders map(CreateOrderDto orderDto);
 
     User findUser(String username) {
@@ -29,5 +33,27 @@ public abstract class OrdersMapper {
                         "User with the username: " + username + " not found "
                 )
         );
+    }
+
+    Double getTotalOrderPrice(CreateOrderDto orderDto) {
+        if (orderDto != null) {
+            Double totalPrice = 0.0;
+//            Iterator<MenuOrders> order = orders.getOrders().iterator();
+//            while (order.hasNext()) {
+//                totalPrice += order.next().getPrice();
+//            }
+            for (MenuOrderDto order : orderDto.getMenuList()) {
+                if (order.getPrice() == null) {
+                    throw new OrderException("order was empty, please re-order again");
+                }
+                totalPrice += order.getPrice();
+            }
+            return totalPrice;
+        }
+        throw new OrderException("Order is null, please re-order again");
+    }
+
+    Date returnDate(){
+        return new Date();
     }
 }
